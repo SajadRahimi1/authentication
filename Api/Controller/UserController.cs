77dd8a8897b326cffe36f4738f2b8ba9a22d6279
Sajjad Rahimi <s.rahimi@vestasoft.local>
2024-05-05 17:06:56 +0330

    change password encryption method

--- Api/Controller/UserController.cs
+++ Api/Controller/UserController.cs
@@ -6,14 +6,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controller;
 
-[ApiController,Route("/api/[controller]")]
+[ApiController, Route("/api/[controller]/[action]")]
 public class UserController(ISender mediator) : ControllerBase
 {
-    [HttpPost(),Route("create"),Authorize(Roles =nameof(RoleEnum.Admin))]
+    [HttpPost, Authorize(Roles = nameof(RoleEnum.Admin))]
     public async Task<CreateUserResponse> CreateUser([FromBody] CreateUserCommand createUserCommand) =>
-       await mediator.Send(createUserCommand);
-    
-    [HttpPost,Route("login")]
-    public async Task<GetUserResponse> CreateUser([FromBody] GetUserCommand getUserCommand) =>
+        await mediator.Send(createUserCommand);
+
+    [HttpPost]
+    public async Task<GetUserResponse> LoginUser([FromBody] GetUserCommand getUserCommand) =>
         await mediator.Send(getUserCommand);
 }
---
+++ Utility/Helper/PasswordHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Utility.Helper;
+
+public class PasswordEncoder
+{
+    public static int keySize = 64;
+    public static int  iterations = 158000;
+    public static string EncodePasswordToBase64(string password, out byte[] salt)
+    {
+        salt = RandomNumberGenerator.GetBytes(keySize);
+        var hash = Rfc2898DeriveBytes.Pbkdf2(Encoding.UTF8.GetBytes(password), salt, iterations,
+            HashAlgorithmName.SHA512, keySize);
+        return Convert.ToHexString(hash);
+    }
+    
+    public static bool VerifyPassword(string password, string hash, byte[] salt)
+    {
+        var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, iterations, HashAlgorithmName.SHA512, keySize);
+        return CryptographicOperations.FixedTimeEquals(hashToCompare, Convert.FromHexString(hash));
+    }
+}
